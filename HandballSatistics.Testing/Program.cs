2de551d6b92b61e6412dffaa7fd429702af46da8
Kaztone <david.brandfellner@hotmail.com>
2022-05-23 23:01:51 +0200

    Refactored program.cs for testing purposes.

--- HandballSatistics.Testing/Program.cs
+++ HandballSatistics.Testing/Program.cs
@@ -12,21 +12,27 @@ namespace HandballStatistics.Testing
     {
         static void Main(string[] args)
         {
+            #region ------------------------------ Service Implementation --------------------------
+
             IDataService<User> dataService = new GenericDataService<User>(
                 new HandballStatisticsDbContext());
 
-            IDataService<Account> accountDataService = new AccountDataService(
+            IAccountDataService accountDataService = new AccountDataService(
                 new HandballStatisticsDbContext());
 
             IAuthenticationService authenticationService = new AuthenticationService(accountDataService);
 
+            #endregion
+
+            #region ------------------------------ GenericDataService ------------------------------
+
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("Test: GenericDataService");
+            Console.WriteLine("--------------------------------------------------\n");
+
             // Check connection GenericDataService
             bool check = dataService.Init().Result;
-            Console.WriteLine($"GenericDataService datbase connection established: {check}{Environment.NewLine}");
-
-            // Check connection AccountDataService
-            bool check2 = accountDataService.Init().Result;
-            Console.WriteLine($"AccountDataService datbase connection established: {check}{Environment.NewLine}");
+            Console.WriteLine($"GenericDataService database connection established: {check}{Environment.NewLine}");           
 
             // Check "Get" GenericDataService
             User user = dataService.Get(1).Result;
@@ -43,23 +49,50 @@ namespace HandballStatistics.Testing
             int numberUsers = dataService.GetAll().Result.Count();
             Console.WriteLine($"Total users:{numberUsers}{Environment.NewLine}");
 
+            Console.WriteLine("--------------------------------------------------");
+
+            #endregion
+
+            #region ------------------------------ AccountDataService ------------------------------
+
+            Console.WriteLine("Test: AccountDataService");
+            Console.WriteLine("--------------------------------------------------\n");
+
+            // Check connection AccountDataService
+            bool check2 = accountDataService.Init().Result;
+            Console.WriteLine($"AccountDataService database connection established: {check}{Environment.NewLine}");
+
             // Check "Get" AccountDataService
             Account account = accountDataService.Get(1).Result;
-            account.AccountHolder = dataService.Get(account.AccountHolderId).Result;
             Console.WriteLine(
                 $"AccountId: {account.Id}\n" +
                 $"AccountHolderId: {account.AccountHolderId}\n" +
                 $"AccountHolderName: {account.AccountHolder.UserName}\n"
             );
 
-            // Check Register
-            //authenticationService.Register("registrationTest", "test@hotmail.com", "registrationTest", "registrationTest");
+            Console.WriteLine("--------------------------------------------------");
 
-            // Check number of Users and Accounts
+            #endregion
+
+            #region ------------------------------ AuthenticationService ---------------------------
+
+            Console.WriteLine("Test: AccountDataService");
+            Console.WriteLine("--------------------------------------------------\n");
+
+            // Check AuthenticationService: Register
+            // authenticationService.Register("registrationTest", "test@hotmail.com", "registrationTest", "registrationTest").Wait();
             Console.WriteLine(
                 $"Total users: {dataService.GetAll().Result.Count()}\n" +
                 $"Total accounts: {accountDataService.GetAll().Result.Count()}\n"
             );
+
+            // Check AuthenticationService: Login
+            account = authenticationService.Login("admin", "admin").Result;
+            Console.WriteLine($"Logged in as: {account.AccountHolder.UserName}\n");
+
+            Console.WriteLine("--------------------------------------------------");
+
+            #endregion
 
             Console.ReadLine();
         }
