df655048dccbd9c02b93133072b54984bf412630
Kaztone <david.brandfellner@hotmail.com>
2022-05-23 12:53:13 +0200

    Added migration InitialCreate.

---
+++ HandballStatistics.EntityFramework/Migrations/202205231051171_InitialCreate.cs
@@ -0,0 +1,29 @@
+﻿namespace HandballStatistics.EntityFramework.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserName = c.String(),
+                        Email = c.String(),
+                        Password = c.String(),
+                        DateJoined = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
