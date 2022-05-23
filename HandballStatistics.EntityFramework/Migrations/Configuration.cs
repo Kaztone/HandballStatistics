4abff9be27990b49beb00bda7dc9fe5bf71375ea
Kaztone <david.brandfellner@hotmail.com>
2022-05-23 12:48:48 +0200

    Refactores seed data for DbSet Users.

---
+++ HandballStatistics.EntityFramework/Migrations/Configuration.cs
@@ -0,0 +1,36 @@
+﻿using HandballStatistics.Domain.Models;
+using HandballStatistics.EntityFramework.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
+namespace HandballStatistics.EntityFramework.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<HandballStatisticsDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(HandballStatisticsDbContext context)
+        {
+            if (context.Users.Any())
+            {
+                return;
+            }
+
+            context.Users.AddRange(new List<User>()
+            {
+                new User()
+                {
+                    UserName = "admin",
+                    Email = "admin@hotmail.com",
+                    Password = "admin",
+                    DateJoined = new DateTime(2022, 3, 23).Date
+                }
+            });
+        }
+    }
+}
