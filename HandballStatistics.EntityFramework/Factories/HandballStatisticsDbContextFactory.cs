4f6f24a754fbd09384ed089916fb61f57757ed96
Kaztone <david.brandfellner@hotmail.com>
2022-05-23 12:47:52 +0200

    Added factory for HandballStatisticsDbContext.

---
+++ HandballStatistics.EntityFramework/Factories/HandballStatisticsDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using HandballStatistics.EntityFramework.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandballStatistics.EntityFramework.Factories
+{
+    public class HandballStatisticsDbContextFactory
+    {
+        public HandballStatisticsDbContext CreateContext()
+        {
+            return new HandballStatisticsDbContext();
+        }
+    }
+}
