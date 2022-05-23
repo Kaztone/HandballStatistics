d3f823862fd9f802774a9716ba432774cf9926d0
Kaztone <david.brandfellner@hotmail.com>
2022-05-23 13:41:50 +0200

    Added GUI to project.

---
+++ HandballStatistics.GUI/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace HandballStatistics.GUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Window window = new MainWindow();
+            window.Show();
+
+            base.OnStartup(e);
+        }
+    }
+}
