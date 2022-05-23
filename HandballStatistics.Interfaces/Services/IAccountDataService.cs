64c38a956673e05f12a48b623170f9e75647e38f
Kaztone <david.brandfellner@hotmail.com>
2022-05-23 16:43:34 +0200

    Added more interfaces to fit mvvm design pattern.

---
+++ HandballStatistics.Interfaces/Services/IAccountDataService.cs
@@ -0,0 +1,17 @@
+﻿using HandballStatistics.Interfaces.Services;
+using HandballStatistics.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandballStatistics.Interfaces.Services
+{
+    public interface IAccountDataService : IDataService<Account>
+    {
+        Task<Account> GetByUsername(string username);
+
+        Task<Account> GetByEmail(string email);
+    }
+}
---
+++ HandballStatistics.Interfaces/Services/IAuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿using HandballStatistics.Models;
+using System.Threading.Tasks;
+
+namespace HandballStatistics.Interfaces.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<bool> Register(string username, string email, string password, string confirmPassword);
+
+        Task<Account> Login(string username, string password);
+    }
+}
