b40d200a39bc130b2c4f0d02fc7493e2f632995f
Kaztone <david.brandfellner@hotmail.com>
2022-05-25 16:53:10 +0200

    Refactored "IAuthenticationService".

--- HandballStatistics.Interfaces/Services/IAuthenticationService.cs
+++ HandballStatistics.Interfaces/Services/IAuthenticationService.cs
@@ -13,10 +13,8 @@ namespace HandballStatistics.Interfaces.Services
 
     public interface IAuthenticationService
     {
-        Task<bool> Register(string username, string email, string password, string confirmPassword);
+        Task<RegistrationResult> Register(string username, string email, string password, string confirmPassword);
 
         Task<Account> Login(string username, string password);
-
-        void Logout();
     }
 }
