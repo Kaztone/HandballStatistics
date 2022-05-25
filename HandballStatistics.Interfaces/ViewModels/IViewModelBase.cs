d0cd11bc4a96cb336828b968ff0b1bdb17822a5f
Kaztone <david.brandfellner@hotmail.com>
2022-05-25 17:07:53 +0200

    Added interface "IViewModelBase".

---
+++ HandballStatistics.Interfaces/ViewModels/IViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandballStatistics.Interfaces.ViewModels
+{
+    public interface IViewModelBase : INotifyPropertyChanged
+    {
+        new event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string propertyName);
+        
+    }
+}
