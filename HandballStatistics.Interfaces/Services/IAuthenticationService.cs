4db05c7bec03f2a2030b5777cb1acc1bc58f2562
Kaztone <david.brandfellner@hotmail.com>
2022-05-25 16:38:43 +0200

    Added enum "RegistrationStatus".

--- HandballStatistics.Interfaces/Services/IAuthenticationService.cs
+++ HandballStatistics.Interfaces/Services/IAuthenticationService.cs
@@ -3,10 +3,20 @@ using System.Threading.Tasks;
 
 namespace HandballStatistics.Interfaces.Services
 {
+    public enum RegistrationResult
+    {
+        Success,
+        PasswordsDoNotMatch,
+        EmailAlreadyExists,
+        UsernameAlrreadyExists
+    }
+
     public interface IAuthenticationService
     {
         Task<bool> Register(string username, string email, string password, string confirmPassword);
 
         Task<Account> Login(string username, string password);
+
+        void Logout();
     }
 }
