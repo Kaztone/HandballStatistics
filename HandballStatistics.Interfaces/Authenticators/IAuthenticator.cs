3624a8451f8f2e6b17d4c8ef1714145f8113775c
Kaztone <david.brandfellner@hotmail.com>
2022-05-25 16:38:10 +0200

    Added interface "IAuthenticator".

---
+++ HandballStatistics.Interfaces/Authenticators/IAuthenticator.cs
@@ -0,0 +1,22 @@
+﻿using HandballStatistics.Interfaces.Services;
+using HandballStatistics.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandballStatistics.Interfaces.Authenticators
+{
+    public interface IAuthenticator
+    {
+        User CurrentUser { get; }
+
+        bool IsLoggedIn { get; }
+
+        Task<RegistrationResult> Register(string username, string email, string password, string confirmPassword);
+
+        Task<bool> Login(string username, string password);
+
+    }
+}
