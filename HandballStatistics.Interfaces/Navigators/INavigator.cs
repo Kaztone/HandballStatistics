59be3d4aa76554bb41647c7645484a2ff582f307
Kaztone <david.brandfellner@hotmail.com>
2022-05-25 17:07:27 +0200

    Added interface "INavigator".

---
+++ HandballStatistics.Interfaces/Navigators/INavigator.cs
@@ -0,0 +1,27 @@
+﻿using HandballStatistics.Interfaces.ViewModels;
+using System.Windows.Input;
+
+namespace HandballStatistics.Interfaces.Navigators
+{
+    public enum ViewType
+    {
+        Login,
+        Home,
+        TrackGame,
+        ViewStats,
+        AddData,
+        AddTeam,
+        DeleteTeam,
+        AddPlayer,
+        DeletePlayer,
+        Profile,
+        Help
+    }
+
+    public interface INavigator
+    {
+        IViewModelBase CurrentViewModel { get; set; }
+
+        ICommand UpdateCurrentViewModelCommand { get; }
+    }
+}
