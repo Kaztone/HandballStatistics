40ac1cf22f5cee902131fbaebd897e0b8c1bc746
Kaztone <david.brandfellner@hotmail.com>
2022-05-26 17:40:52 +0200

    Reworked controls and added views.

--- HandballStatistics.GUI/App.xaml.cs
+++ HandballStatistics.GUI/App.xaml.cs
@@ -1,6 +1,14 @@
-﻿using HandballStatistics.Interfaces.Services;
+﻿using HandballStatistics.EntityFramework.Contexts;
+using HandballStatistics.Interfaces.Authenticators;
+using HandballStatistics.Interfaces.Navigators;
+using HandballStatistics.Interfaces.Services;
+using HandballStatistics.Interfaces.ViewModels;
 using HandballStatistics.Models;
+using HandballStatistics.Services.AuthenticationServices;
 using HandballStatistics.Services.DbServices;
+using HandballStatistics.Services.Factories;
+using HandballStatistics.Services.Navigators;
+using HandballStatistics.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Windows;
@@ -17,6 +25,7 @@ namespace HandballStatistics.GUI
             IServiceProvider serviceProvider = CreateServiceProvider();
 
             Window window = new MainWindow();
+            window.DataContext = serviceProvider.GetRequiredService<MainViewModel>();
             window.Show();
 
             base.OnStartup(e);
@@ -25,6 +34,31 @@ namespace HandballStatistics.GUI
         private IServiceProvider CreateServiceProvider()
         {
             IServiceCollection services = new ServiceCollection();
+
+            // Implemented services
+            services.AddSingleton<HandballStatisticsDbContext>();
+            services.AddSingleton<IAuthenticationService, AuthenticationService>();
+            services.AddSingleton<IDataService<Account>, AccountDataService>();
+            services.AddSingleton<IAccountDataService, AccountDataService>();
+
+            // ViewModels as services
+            services.AddSingleton<IViewModelAbstractFactory, ViewModelAbstractFactory>();
+            services.AddSingleton<IViewModelFactory<HomeViewModel>, HomeViewModelFactory>();
+            services.AddSingleton<IViewModelFactory<TrackGameViewModel>, TrackGameViewModelFactory>();
+            services.AddSingleton<IViewModelFactory<ViewStatsViewModel>, ViewStatsViewModelFactory>();
+            services.AddSingleton<IViewModelFactory<ConfigurationViewModel>, ConfigurationViewModelFactory>();
+            services.AddSingleton<IViewModelFactory<ProfileViewModel>, ProfileViewModelFactory>();
+            services.AddSingleton<IViewModelFactory<HelpViewModel>, HelpViewModelFactory>();
+
+            services.AddSingleton<IViewModelFactory<LoginViewModel>>((s) => 
+                new LoginViewModelFactory(s.GetRequiredService<IAuthenticator>(), 
+                new ViewModelFactoryRenavigator<HomeViewModel>(s.GetRequiredService<INavigator>(),
+                s.GetRequiredService<IViewModelFactory<HomeViewModel>>())));
+
+            // Single instance services
+            services.AddScoped<IAuthenticator, Authenticator>();
+            services.AddScoped<INavigator, Navigator>();
+            services.AddScoped<MainViewModel>();
 
             return services.BuildServiceProvider();
         }
