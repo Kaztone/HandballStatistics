2ec49fc971a475c6ce9b409cb2d10e3776299360
Kaztone <david.brandfellner@hotmail.com>
2022-05-26 17:38:13 +0200

    Refactored views.

---
+++ HandballStatistics.GUI/Views/LoginView.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HandballStatistics.GUI.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : UserControl
+    {
+        public static readonly DependencyProperty LoginCommandProperty =
+            DependencyProperty.Register("LoginCommand", typeof(ICommand), typeof(LoginView), new PropertyMetadata(null));
+
+        public ICommand LoginCommand
+        {
+            get { return (ICommand)GetValue(LoginCommandProperty); }
+            set { SetValue(LoginCommandProperty, value); }
+        }
+
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if(LoginCommand != null)
+            {
+                string password = pbPassword.Password;
+                LoginCommand.Execute(password);
+            }
+            
+        }
+    }
+}
