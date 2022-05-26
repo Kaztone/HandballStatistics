5e25a943e4fdfe1bb6920c5c873b7a8565246176
Kaztone <david.brandfellner@hotmail.com>
2022-05-26 17:35:43 +0200

    Reworked Navigator.

---
+++ HandballStatistics.Services/Navigators/Navigator.cs
@@ -0,0 +1,23 @@
+﻿using HandballStatistics.Commands;
+using HandballStatistics.Interfaces.Navigators;
+using HandballStatistics.Interfaces.ViewModels;
+using HandballStatistics.Models;
+using System.Windows.Input;
+
+namespace HandballStatistics.Services.Navigators
+{
+    public class Navigator : ObservableObject, INavigator
+    {
+        private IViewModelBase currentViewModel;
+
+        public IViewModelBase CurrentViewModel
+        {
+            get => this.currentViewModel;
+            set
+            {
+                this.currentViewModel = value;
+                OnPropertyChanged(nameof(CurrentViewModel));
+            }
+        }
+    }
+}
