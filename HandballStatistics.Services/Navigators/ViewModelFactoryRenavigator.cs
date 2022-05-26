513f6b075db03a74060e40b92a4bc3ebc551a364
Kaztone <david.brandfellner@hotmail.com>
2022-05-26 17:35:21 +0200

    Added ViewModelRenavigator.

---
+++ HandballStatistics.Services/Navigators/ViewModelFactoryRenavigator.cs
@@ -0,0 +1,24 @@
+﻿using HandballStatistics.Interfaces.Navigators;
+using HandballStatistics.Interfaces.ViewModels;
+using System;
+
+namespace HandballStatistics.Services.Navigators
+{
+    public class ViewModelFactoryRenavigator<TViewModel> : IRenavigator where TViewModel : IViewModelBase
+    {
+        private readonly INavigator navigator;
+
+        private readonly IViewModelFactory<TViewModel> viewModelFactory;
+
+        public ViewModelFactoryRenavigator(INavigator navigator, IViewModelFactory<TViewModel> viewModelFactory)
+        {
+            this.navigator = navigator;
+            this.viewModelFactory = viewModelFactory;
+        }
+
+        public void Renavigate()
+        {
+            this.navigator.CurrentViewModel = this.viewModelFactory.CreateViewModel();
+        }
+    }
+}
